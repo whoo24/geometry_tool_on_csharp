c0b692de0fcb327e467d5fded7faa476d729feff
whoo24 <whoo24@gmail.com>
2012-08-06 17:58:16 +0900

    Matrix class and Vector class added
    행렬 클래스와 벡터 클래스 추가

---
+++ GeometryTool/Matrix.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeometryTool
+{
+    class Matrix
+    {
+        double[,] m_m = new double[3,3];
+
+        public void SetScale(double sx, double sy, double sz)
+        {
+            m_m[0,0] = sx;
+            m_m[1,1] = sy;
+            m_m[2,2] = sz;
+        }
+
+        public Vector RowToVec(int row_index)
+        {
+            return new Vector { X = m_m[row_index, 0], Y = m_m[row_index, 1], Z = m_m[row_index, 2] };
+        }
+
+        public void VecToRow(int row_index, Vector v)
+        {
+            m_m[row_index, 0] = v.X;
+            m_m[row_index, 1] = v.Y;
+            m_m[row_index, 2] = v.Z;
+        }
+
+        public static Matrix Multiply(Matrix a, Matrix b)
+        {
+            Matrix r = new Matrix();
+
+            r.VecToRow(0, Matrix.Multiply(a, b.RowToVec(0)));
+            r.VecToRow(1, Matrix.Multiply(a, b.RowToVec(1)));
+            r.VecToRow(2, Matrix.Multiply(a, b.RowToVec(2)));
+
+            return r;
+        }
+
+        public static Vector Multiply(Matrix m, Vector v)
+        {
+            Vector r = new Vector{
+                X = (m.m_m[0, 0] * v.X) + (m.m_m[0, 1] * v.Y) + (m.m_m[0, 2] * v.Z),
+                Y = (m.m_m[1, 0] * v.X) + (m.m_m[1, 1] * v.Y) + (m.m_m[1, 2] * v.Z),
+                Z = (m.m_m[2, 0] * v.X) + (m.m_m[2, 1] * v.Y) + (m.m_m[2, 2] * v.Z),
+            };
+
+            return r;
+        }
+
+        
+    }
+}
+
---
+++ GeometryTool/Vector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeometryTool
+{
+    class Vector
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+    }
+}
